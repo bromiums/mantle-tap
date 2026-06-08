bac11098e14904cf3f4768fa22b5d0432a1a76cd
faisalganteng1265 <faisalakmal2105@gmail.com>
2026-06-08 20:00:17 +0700

    Centralize Mantle Sepolia RPC config

--- fe/src/config/chains.ts
+++ fe/src/config/chains.ts
@@ -1,11 +1,14 @@
 import { defineChain } from 'viem';
 
+export const MANTLE_SEPOLIA_RPC =
+  process.env.NEXT_PUBLIC_RPC_URL || 'https://5003.rpc.thirdweb.com';
+
 export const mantleSepolia = defineChain({
   id: 5003,
   name: 'Mantle Sepolia',
   nativeCurrency: { name: 'MNT', symbol: 'MNT', decimals: 18 },
   rpcUrls: {
-    default: { http: ['https://rpc.sepolia.mantle.xyz'] },
+    default: { http: [MANTLE_SEPOLIA_RPC] },
   },
   blockExplorers: {
     default: { name: 'Mantle Sepolia Explorer', url: 'https://explorer.sepolia.mantle.xyz' },
